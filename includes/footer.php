5fab54c882f6dc26c13f7bf8f4174b9318576032
Satwik Anand Chauhan <93284505+satwikanandchauhan@users.noreply.github.com>
2022-09-25 14:57:30 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">PG in Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">PG in Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">PG in Hyderabad</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2022 All rights reserved PG Life. 
+            <br>Made with ❣️ by <a href="https://satwik-portfolio.netlify.app/" target="_blank"> Satwik</a> 
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
